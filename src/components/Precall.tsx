b9c7a08ccda80b4eed20a159bdd7b18f10fc76f6
EkaanshArora <ekaansh68@gmail.com>
2021-11-18 00:56:19 +0530

    fix snap and match

--- src/components/Precall.tsx
+++ src/components/Precall.tsx
@@ -9,45 +9,6 @@
  information visit https://appbuilder.agora.io.
 *********************************************
 */
-function b64ToUint6(nChr) {
-  // convert base64 encoded character to 6-bit integer
-  // from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
-  return nChr > 64 && nChr < 91
-    ? nChr - 65
-    : nChr > 96 && nChr < 123
-    ? nChr - 71
-    : nChr > 47 && nChr < 58
-    ? nChr + 4
-    : nChr === 43
-    ? 62
-    : nChr === 47
-    ? 63
-    : 0;
-}
-function base64DecToArr(sBase64, nBlocksSize) {
-  var sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ''),
-    nInLen = sB64Enc.length,
-    nOutLen = nBlocksSize
-      ? Math.ceil(((nInLen * 3 + 1) >> 2) / nBlocksSize) * nBlocksSize
-      : (nInLen * 3 + 1) >> 2,
-    taBytes = new Uint8Array(nOutLen);
-
-  for (
-    var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0;
-    nInIdx < nInLen;
-    nInIdx++
-  ) {
-    nMod4 = nInIdx & 3;
-    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << (18 - 6 * nMod4);
-    if (nMod4 === 3 || nInLen - nInIdx === 1) {
-      for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
-        taBytes[nOutIdx] = (nUint24 >>> ((16 >>> nMod3) & 24)) & 255;
-      }
-      nUint24 = 0;
-    }
-  }
-  return taBytes;
-}
 
 import React, {useState, useContext} from 'react';
 import {
@@ -234,13 +195,9 @@ const Precall = (props: any) => {
                 </Text>
                 <canvas
                   id="preview"
-                  width="640"
+                  width="848"
                   height="480"
-                  style={{
-                    display: snapped ? 'block' : 'none',
-                    width: 400,
-                    height: 200,
-                  }}
+                  style={{display: snapped ? 'block' : 'none', width: 424, height: 240}}
                 />
               </>
             )}
@@ -257,7 +214,7 @@ const Precall = (props: any) => {
                   <View style={{marginBottom: 20}} />
                   <PrimaryButton
                     onPress={() => {
-                      snap(
+                      window.AgoraProctorUtils.snap(
                         document.getElementsByTagName('video')[0],
                         document.getElementById('preview'),
                       ).then(function (result) {
